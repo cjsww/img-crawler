beb878ff135f2592f87ec9f3ee131d78f05b6471
chojinwook <chojinwook2010@gmail.com>
2025-07-15 19:42:00 +0900

    같은파일에 저장하도록 수정

--- crawler.py
+++ crawler.py
@@ -1,23 +1,41 @@
+import os
+import shutil
 from icrawler.builtin import GoogleImageCrawler
 
 presidents = [
-    "이승만 대통령 얼굴 사진",
-    "윤보선 대통령 얼굴 사진",
-    "박정희 대통령 얼굴 사진",
-    "최규하 대통령 얼굴 사진",
-    "전두환 대통령 얼굴 사진",
-    "노태우 대통령 얼굴 사진",
-    "김영삼 대통령 얼굴 사진",
-    "김대중 대통령 얼굴 사진",
-    "노무현 대통령 얼굴 사진",
-    "이명박 대통령 얼굴 사진",
-    "박근혜 대통령 얼굴 사진",
-    "문재인 대통령 얼굴 사진",
-    "윤석열 대통령 얼굴 사진"
-    "이재명 대통령 얼굴 사진"
+    "이승만",
+    "윤보선",
+    "박정희",
+    "최규하",
+    "전두환",
+    "노태우",
+    "김영삼",
+    "김대중",
+    "노무현",
+    "이명박",
+    "박근혜",
+    "문재인",
+    "윤석열",
+    "이재명"
 ]
 
-for name in presidents:
-    folder = name.split()[0]  # 이름만 폴더 이름으로
-    crawler = GoogleImageCrawler(storage={'root_dir': folder})
-    crawler.crawl(keyword=name, max_num=50)
+final_folder = 'all_presidents'
+os.makedirs(final_folder, exist_ok=True)
+
+for keyword in presidents:
+    name = keyword.split()[0]
+    temp_folder = f'_tmp_{name}'
+    os.makedirs(temp_folder, exist_ok=True)
+
+    print(f'크롤링 시작: {name}')
+    crawler = GoogleImageCrawler(storage={'root_dir': temp_folder})
+    crawler.crawl(keyword=keyword, max_num=10)
+
+    # 임시폴더 이미지 → 최종폴더로 이름 붙여서 이동
+    for idx, fname in enumerate(sorted(os.listdir(temp_folder))):
+        src = os.path.join(temp_folder, fname)
+        dst_name = f'{name}_{idx+1}.jpg'
+        dst = os.path.join(final_folder, dst_name)
+        shutil.move(src, dst)
+
+    shutil.rmtree(temp_folder)
