cabeb030238e77ae13baa9361423427dd07d6dd3
chojinwook <chojinwook2010@gmail.com>
2025-07-15 19:32:39 +0900

    폴더별 정리

--- crawler.py
+++ crawler.py
@@ -1,4 +1,23 @@
 from icrawler.builtin import GoogleImageCrawler
 
-crawler = GoogleImageCrawler(storage={'root_dir': '문재인'})
-crawler.crawl(keyword='문재인 대통령 얼굴 사진', max_num=20)
+presidents = [
+    "이승만 대통령 얼굴 사진",
+    "윤보선 대통령 얼굴 사진",
+    "박정희 대통령 얼굴 사진",
+    "최규하 대통령 얼굴 사진",
+    "전두환 대통령 얼굴 사진",
+    "노태우 대통령 얼굴 사진",
+    "김영삼 대통령 얼굴 사진",
+    "김대중 대통령 얼굴 사진",
+    "노무현 대통령 얼굴 사진",
+    "이명박 대통령 얼굴 사진",
+    "박근혜 대통령 얼굴 사진",
+    "문재인 대통령 얼굴 사진",
+    "윤석열 대통령 얼굴 사진"
+    "이재명 대통령 얼굴 사진"
+]
+
+for name in presidents:
+    folder = name.split()[0]  # 이름만 폴더 이름으로
+    crawler = GoogleImageCrawler(storage={'root_dir': folder})
+    crawler.crawl(keyword=name, max_num=50)
